588d82810cc7ca629932ac49d6e46af1e7eb27de
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-12-07 16:20:43 -0500

    Updates

---
+++ precache-manifest.2c5e6a2390efe8101bab10827476e9b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f856fed5aae397dda183e527e9cb25b",
+    "url": "/Spotify-Insight/index.html"
+  },
+  {
+    "revision": "ec4face7ddf5510fd4cb",
+    "url": "/Spotify-Insight/static/css/main.1ae3b44d.chunk.css"
+  },
+  {
+    "revision": "cbbadbbdbb4ab54e4241",
+    "url": "/Spotify-Insight/static/js/2.20a6abcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spotify-Insight/static/js/2.20a6abcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4face7ddf5510fd4cb",
+    "url": "/Spotify-Insight/static/js/main.0d6ddd9e.chunk.js"
+  },
+  {
+    "revision": "6a7b8fbf192598e7e0a3",
+    "url": "/Spotify-Insight/static/js/runtime-main.28381178.js"
+  },
+  {
+    "revision": "fd18543bef9de9316d954eafe7a33270",
+    "url": "/Spotify-Insight/static/media/cat-5.fd18543b.svg"
+  },
+  {
+    "revision": "664c716d93b55fd3da5b7ad99f390867",
+    "url": "/Spotify-Insight/static/media/dj.664c716d.svg"
+  },
+  {
+    "revision": "563190f555f11d22618aa2ae1b366226",
+    "url": "/Spotify-Insight/static/media/sleep.563190f5.jpg"
+  }
+]);
